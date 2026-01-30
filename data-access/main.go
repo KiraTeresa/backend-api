4c5446dc49ec6aaf51fb15ac39ec31c0b71ded73
KiraTeresa <kira.soba@gmx.de>
2026-01-30 08:52:44 +0100

    data-access: implement albumById to query for a single row

--- data-access/main.go
+++ data-access/main.go
@@ -54,6 +54,12 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Albums found: %v\n", albums)
+
+	alb, err := albumById(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
 }
 
 type Album struct {
@@ -87,4 +93,20 @@ func albumsByArtist(name string) ([]Album, error) {
 	}
 
 	return albums, nil
+}
+
+// albumById queries for the album with the specified ID
+func albumById(id int64) (Album, error) {
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	// Scan copies the columns from the matched row into the values pointed at by dest.
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumById %d: %v", id, err)
+	}
+
+	return alb, nil
 }
