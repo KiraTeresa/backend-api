8395f737b9e03a295340979b935c33e1303e025e
KiraTeresa <kira.soba@gmx.de>
2026-01-30 08:45:20 +0100

    data-access: implement albumsByArtist to query for multiple rows

--- data-access/main.go
+++ data-access/main.go
@@ -48,4 +48,43 @@ func main() {
 	}
 
 	fmt.Println("-- CONNECTED --")
+
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+}
+
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
+// albumsByArtist queries for albums that have the specified artist name
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var alb Album
+		// Scan copies the columns from the matched row into the values pointed at by dest.
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	return albums, nil
 }
